e51dcdfe432e2aa8026a61e0c759e9b539f75f15
WillemGibson <willemjackgibson@gmail.com>
2024-03-15 20:23:51 +1100

    Adding register and login functionality

--- main.py
+++ main.py
@@ -18,4 +18,7 @@ def create_app():
     from controllers.cli_controller import db_commands
     app.register_blueprint(db_commands)
 
+    from controllers.auth_controller import auth_bp
+    app.register_blueprint(auth_bp)
+
     return app
